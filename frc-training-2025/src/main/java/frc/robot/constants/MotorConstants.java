932049f3f733eac3a4d256599e023c9244ddefae
Nathaniel Wilson <nathaniel@Nathaniels-MacBook-Air.local>
2025-10-16 18:00:50 -0700

    updating-motor

---
+++ frc-training-2025/src/main/java/frc/robot/constants/MotorConstants.java
@@ -0,0 +1,11 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.constants;
+
+/** Add your docs here. */
+public class MotorConstants {
+    public final static int kMotorID = 16;
+    public final static double kMultiplier = 0.5;
+}
